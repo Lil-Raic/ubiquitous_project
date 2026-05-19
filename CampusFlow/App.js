bd0ffbd647c8cb5c0007f0aa079510b9288f94b0
Lil-Raic <masterraicone@gmail.com>
2026-05-19 18:35:26 +0200

    structure

--- CampusFlow/App.js
+++ CampusFlow/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
-import { colors } from './src/theme/colors';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
-      <StatusBar barStyle="dark-content" />
-      <LoginScreen />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
